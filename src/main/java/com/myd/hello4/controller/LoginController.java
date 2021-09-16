2f5c58dce0f29ba8477cd243829ed187d1085351
OF3787 <mayuanding@qianmi.com>
2021-09-16 10:03:18 +0800

    *自定义注解封装异常

--- src/main/java/com/myd/hello4/controller/LoginController.java
+++ src/main/java/com/myd/hello4/controller/LoginController.java
@@ -16,7 +16,7 @@ import java.util.List;
  * @author <a href="mailto:mayuanding@qianmi.com">OF3787-马元丁</a>
  * @version 0.1.0
  * @Date:2021/3/2 10:03
- * @Description: TODO
+ * @Description:
  */
 @RestController
 public class LoginController {
